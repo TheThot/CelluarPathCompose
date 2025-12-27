379d3e75c96d6853332b75707788b0d149f94b33
torrpathian <torrpathian@gmail.com>
2025-12-27 12:15:11 +0300

    small fix

--- libs/a-star/main.cpp
+++ libs/a-star/main.cpp
@@ -4,15 +4,17 @@
 int main()
 {
     AStar::Generator generator;
-    generator.setWorldSize({25, 25});
+    int sizeX = 25, sizeY = 25;
+    generator.setWorldSize({sizeX, sizeY});
     generator.setHeuristic(AStar::Heuristic::manhattan);
     generator.setDiagonalMovement(true);
 
     // Добавляем различные препятствия
     std::cout << "=== Тест 1: Диагональная стена ===\n";
-    int j = 20;
+    int coeff = std::max(sizeX, sizeY)*0.2;
+    int j = sizeY-coeff;
     for(int i = 0; i < j; ++i) {
-        if(i != j-5)
+        if(i != j-coeff)
             generator.addCollision({i, j - 1 - i});
     }
 
@@ -35,33 +37,36 @@ int main()
     generator.addCollision({9, 9});*/
 
     // Ищем путь
-    std::cout << "Поиск пути из (0,0) в (20,20)...\n";
-    auto path = generator.findPath({0, 0}, {20, 20});
+    std::vector<std::string> args;
+    args.push_back(std::to_string(sizeX));
+    args.push_back(std::to_string(sizeY));
+    std::cout << "Поиск пути из (0,0) в (" << args[0] << "," << args[1] << ")..." << std::endl;
+    auto path = generator.findPath({0, 0}, {sizeX-coeff, sizeY-coeff});
 
     // Визуализируем результат
     //generator.visualizePath(path, {0, 0}, {20, 20});
-    generator.visualizePathColor(path, {0, 0}, {20, 20});
+    generator.visualizePathColor(path, {0, 0}, {sizeX-coeff, sizeY-coeff});
 
     // Тест 2: Непроходимый путь
     std::cout << "\n\n=== Тест 2: Полностью заблокированный путь ===\n";
     generator.clearCollisions();
 
     // Создаем непроходимую стену
-    for(int i = 0; i < 25; ++i) {
-        generator.addCollision({10, i});
+    for(int i = 0; i < sizeX; ++i) {
+        generator.addCollision({sizeY/2, i});
     }
 
-    auto path2 = generator.findPath({0, 0}, {20, 0});
+    auto path2 = generator.findPath({0, 0}, {sizeX-coeff, sizeY-coeff});
     //generator.visualizePath(path2, {0, 0}, {20, 0});
-    generator.visualizePathColor(path2, {0, 0}, {20, 0});
+    generator.visualizePathColor(path2, {0, 0}, {sizeX-coeff, sizeY-coeff});
 
     // Тест 3: Простой путь без препятствий
     std::cout << "\n\n=== Тест 3: Путь без препятствий ===\n";
     generator.clearCollisions();
 
-    auto path3 = generator.findPath({2, 2}, {22, 22});
+    auto path3 = generator.findPath({2, 2}, {sizeX-coeff, sizeY-coeff});
     //generator.visualizePath(path3, {2, 2}, {22, 22});
-    generator.visualizePathColor(path3, {2, 2}, {22, 22});
+    generator.visualizePathColor(path3, {2, 2}, {sizeX-coeff, sizeY-coeff});
 
     return 0;
 }
